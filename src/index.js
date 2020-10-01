99b0cb8dc070b9c157c584a75e175bff4f21ae63
careerdimasprayoga <career.dimasprayoga@gmail.com>
2020-10-01 18:47:51 +0700

    Reupload patch profile & personal + patch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const route = require("express").Router()
 
 // Route
-
+const profile = require("./routes/profile")
 // Middle
-
+route.use("/profile", profile)
 
 module.exports = route
---
+++ src/model/profile.js
@@ -0,0 +1,55 @@
+const connection = require("../config/mysql");
+
+module.exports = {
+
+  getUserById: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT first_name, last_name, phone, (CASE WHEN image = '' THEN 'blank-user.png' ELSE image END) AS image FROM users WHERE id = ?", id, (error, result) => {
+        !error ? resolve(result) : reject(new Error(error))
+      })
+    })
+  },
+  getUserFullById: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT first_name, last_name, email, phone, (CASE WHEN image = '' THEN 'blank-user.png' ELSE image END) AS image, status, created, balance, reset_key FROM users WHERE id = ?", id, (error, result) => {
+        !error ? resolve(result) : reject(new Error(error))
+      })
+    })
+  },
+  getPhone: (phone) => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT phone FROM users WHERE phone = ?", phone, (error, result) => {
+        !error ? resolve(result) : reject(new Error(error))
+      })
+    })
+  },
+  getImage: (id) => {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT id, image FROM users WHERE id = ?", id, (error, result) => {
+        !error ? resolve(result) : reject(new Error(error))
+      })
+    })
+  },
+  patchPersonal: (id, setData) => {
+    return new Promise((resolve, reject) => {
+      connection.query("UPDATE users SET ? WHERE id = ?", [setData, id], (error, result) => {
+        if (!error) {
+          resolve(result)
+        } else {
+          reject(new Error(error))
+        }
+      })
+    })
+  },
+  patchProfileImage: (id, setData) => {
+    return new Promise((resolve, reject) => {
+      connection.query("UPDATE users SET ? WHERE id = ?", [setData, id], (error, result) => {
+        if (!error) {
+          resolve(result)
+        } else {
+          reject(new Error(error))
+        }
+      })
+    })
+  }
+}
