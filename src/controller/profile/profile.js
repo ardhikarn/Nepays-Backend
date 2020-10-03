3576abcfdaf9865987cd04c567439622148bb114
careerdimasprayoga <career.dimasprayoga@gmail.com>
2020-10-03 14:37:28 +0700

    Edit profile request.params

--- src/controller/profile/profile.js
+++ src/controller/profile/profile.js
@@ -6,7 +6,7 @@ module.exports = {
 
   get_profile: async (request, response) => {
     try {
-      const { id_user_login } = request.body
+      const id_user_login = request.params.id
       const result = await getUserById(id_user_login)
       if (result.length > 0) {
         return helper.response(response, 200, 'Get Profile success', result)
@@ -19,7 +19,7 @@ module.exports = {
   },
   get_personal: async (request, response) => {
     try {
-      const { id_user_login } = request.body
+      const id_user_login = request.params.id
       const result = await getUserFullById(id_user_login)
       if (result.length > 0) {
         return helper.response(response, 200, 'Get Personal success', result)
@@ -32,7 +32,8 @@ module.exports = {
   },
   patch_personal_name: async (request, response) => {
     try {
-      const { id_user_login, first_name, last_name } = request.body
+      const id_user_login = request.params.id
+      const { first_name, last_name } = request.body
       const setData = {
         first_name: first_name,
         last_name: last_name
@@ -49,7 +50,8 @@ module.exports = {
   },
   patch_personal_phone: async (request, response) => {
     try {
-      const { id_user_login, phone } = request.body
+      const id_user_login = request.params.id
+      const { phone } = request.body
       const setData = {
         phone: phone
       }
@@ -68,7 +70,8 @@ module.exports = {
   },
   patch_profile_image: async (request, response) => {
     try {
-      let { id_user_login, image } = request.body
+      const id_user_login = request.params.id
+      let { image } = request.body
       const id_user = await getImage(id_user_login)
 
       if (!request.file) {
@@ -83,7 +86,7 @@ module.exports = {
         image: image
       }
       const edit_image = await patchProfileImage(id_user_login, setData)
-      return helper.response(response, 200, 'Update profile success', edit_image)
+      return helper.response(response, 200, 'Update Image success')
     } catch (error) {
       return helper.response(response, 400, 'Bad Request', error)
     }
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,10 +2,10 @@ const router = require('express').Router()
 const uploadImage = require('../middleware/multer')
 const { get_profile, get_personal, patch_personal_name, patch_personal_phone, patch_profile_image } = require('../controller/profile/profile')
 
-router.post('/profile', get_profile)
-router.post('/personal', get_personal)
-router.patch('/profile_image', uploadImage, patch_profile_image)
-router.patch('/personal_name', patch_personal_name)
-router.patch('/personal_phone', patch_personal_phone)
+router.get('/profile/:id', get_profile)
+router.get('/personal/:id', get_personal)
+router.patch('/profile_image/:id', uploadImage, patch_profile_image)
+router.patch('/personal_name/:id', patch_personal_name)
+router.patch('/personal_phone/:id', patch_personal_phone)
 
 module.exports = router
